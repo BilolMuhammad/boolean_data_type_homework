5ea9ab9893b4824654dcb9ec49680cf58437c48b
PRESTIGE <mamatqulov@gmail.com>
2023-01-03 14:39:36 +0500

    08

--- bool08.py
+++ bool08.py
@@ -7,7 +7,7 @@ def main(a):
         bool
     """
     # Write your code here
-    return a >= 0 or a % (a//1) == 0
+    return a == 0 or (a > 0 and a % (a//1) == 0)
 
 
-print(main(4))
+print(main(-3))
